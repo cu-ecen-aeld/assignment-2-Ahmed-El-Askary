2c7dd1c214008bd314b537089a1c99b8caf1a703
Ahmed-El-Askary <ahmedalaskary991@gmail.com>
2024-03-25 02:07:12 +0200

    com2

---
+++ finder-app/writer.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Check if the required arguments are provided
+if [ $# -lt 2 ]; then
+    echo "Error: Two arguments are required."
+    exit 1
+fi
+
+# Assign the command line arguments to variables
+writefile="$1"
+writestr="$2"
+
+# Check if writefile argument is specified
+if [ -z "$writefile" ]; then
+    echo "Error: writefile argument is not specified."
+    exit 1
+fi
+
+# Check if writestr argument is specified
+if [ -z "$writestr" ]; then
+    echo "Error: writestr argument is not specified."
+    exit 1
+fi
+
+# Create the directory path if it doesn't exist
+mkdir -p "$(dirname "$writefile")"
+
+# Write the content to the file
+echo "$writestr" > "$writefile"
+
+# Check if the file was created successfully
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to create the file."
+    exit 1
+fi
+
+# Print success message
+echo "File created: $writefile"
+
+
+if [[ $# -eq 0 ]] ; then
+    echo 'some message'
+    exit 0
+fi
+
+case "$1" in
+    1) echo 'you gave 1' ;;
+    *) echo 'you gave something else' ;;
+esac
